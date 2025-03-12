9f7047cca96dcabc4a613873d79d5f30262c3c6c
ashin <ashinashin@gmail.com>
2025-03-12 14:18:12 +0530

    multiline commands are written

--- commands/max.java
+++ commands/max.java
@@ -2,6 +2,14 @@ import java.util.*;
 public class ashin{
     public static void main(string args[])
     {
+        /*
+        create acommands folder
+        create ashin file
+        initialise variable numone
+        print the variable numone
+
+        */
+    
         int numone=67; // initialise the variable numone
         system.out.println(numone);// print the variable
         system.out.println();
