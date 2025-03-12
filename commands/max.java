b4d639219b43c483601e144998f68b3117ae89f2
ashin <ashinashin@gmail.com>
2025-03-12 14:14:11 +0530

    commands adde

---
+++ commands/max.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class ashin{
+    public static void main(string args[])
+    {
+        int numone=67; // initialise the variable numone
+        system.out.println(numone);// print the variable
+        system.out.println();
+
+    }
+}
+}
