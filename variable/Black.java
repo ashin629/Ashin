2fd4cc9f3b42714a36a7d1f85c66e36832a1f8cf
ashin <ashinashin@gmail.com>
2025-03-12 13:59:14 +0530

    wiselin

---
+++ variable/Black.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class ashin{
+    public static void main(string args[])
+    {
+        int numone=67;
+        system.out.println(numone);
+        system.out.println();
+        
+    }
+}
+}
