b21d5fd5d751abc1a7d6aecf44eb7bcfc3f84ccd
ashin <ashinashin@gmail.com>
2025-03-14 12:16:41 +0530

    hrtyr

---
+++ index.java
@@ -0,0 +1,12 @@
+public class index {
+      
+        public static void main(String[] args) {
+        String name="maddy,king";
+        
+        System.out.println(name.indexOf("a"));
+        System.out.println(name.indexOf("n"));
+           
+    }
+    
+}
+
