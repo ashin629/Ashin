6c8ca8a54f7845e20d43c7ec7484da5bfd89327e
ashin <ashinashin@gmail.com>
2025-03-14 11:48:50 +0530

    fvgdhb

---
+++ char/char1.java
@@ -0,0 +1,6 @@
+public class char1{
+    public static void main(String[] args) {
+        String name="maddy";
+        System.out.println(name.charAt(0));       
+    }
+}
---
+++ char1/char1.java
@@ -0,0 +1,6 @@
+public class char1{
+    public static void main(String[] args) {
+        String name="ashin";
+        System.out.println(name.charAt(0));       
+    }
+}
